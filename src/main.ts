45ab59f3dff85a6c2b5b6f013093f83c95c5c1d5
Corpella <alessandrocordella97@gmail.com>
2024-09-01 01:26:48 +0200

    ✨ feat: add chart library

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from "vue";
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from "./App.vue";
-
+import App from './App.vue'
 
 import 'virtual:uno.css'
 import '@unocss/reset/tailwind.css'
+import 'vue-data-ui/style.css'
 
-createApp(App).use(createPinia()).mount("#app");
+createApp(App).use(createPinia()).mount('#app')
