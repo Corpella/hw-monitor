cf133a938525c08d9ef7972bb23b318949c58f39
Corpella <alessandrocordella97@gmail.com>
2024-09-01 01:28:25 +0200

    ✨ feat: add base chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { VueUiSparkline } from 'vue-data-ui'
 import { useHardware } from '../store/useHardware'
 
-const statsStore = useHardware()
+import { getChartConfig } from '../config/chart.config'
+
+const { stats } = storeToRefs(useHardware())
 </script>
 
 <template>
-  <pre>
-    {{ statsStore.stats }}
-  </pre>
+  <div
+    class="w-full h-screen p-10 grid grid-cols-3 gap-4"
+    v-if="Object.keys(stats).length"
+  >
+    <VueUiSparkline
+      v-for="(data, name) of stats"
+      :key="name"
+      :config="getChartConfig({ text: name, suffix: '°' })"
+      :dataset="data"
+    />
+  </div>
+
+  <!-- <pre>
+    {{ stats }}
+  </pre> -->
 </template>
