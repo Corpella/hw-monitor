8380c3ae0bcbc0909491e6560d1c13edd3833935
Corpella <alessandrocordella97@gmail.com>
2024-08-31 16:25:28 +0200

    ✨ feat: add polling rate as param

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,15 +4,13 @@
 use std::ffi::CStr;
 use std::ffi::CString;
 
-// use std::{thread::sleep, time::Duration};
-use tauri::Manager;
+use tauri::Window;
 
 // extern "C" {
 //     fn GetHardwareInfo(key: *const i8) -> *const i8;
 // }
 
 fn call_dynamic(key: *const i8) -> Result<*const i8, Box<dyn std::error::Error>> {
-    print!("Enter function declaration");
     unsafe {
         let lib = libloading::Library::new("./native-library/WindowsMonitor.dll")?;
         let func: libloading::Symbol<unsafe extern "C" fn(key: *const i8) -> *const i8> =
@@ -25,50 +23,42 @@ fn call_dynamic(key: *const i8) -> Result<*const i8, Box<dyn std::error::Error>>
 struct Payload {
     message: String,
 }
+
+#[tauri::command]
+fn init_process(polling_rate: u64, window: Window) {
+    std::thread::spawn(move || loop {
+        let c_string = CString::new("Cpu,GpuNvidia").unwrap();
+
+        // let result = GetHardwareInfo(c_string.as_ptr());
+        let result = call_dynamic(c_string.as_ptr()).unwrap();
+
+        let c_str = unsafe { CStr::from_ptr(result) };
+        let rust_string = c_str.to_str().unwrap().to_owned();
+        // println!("{:?}", rust_string);
+        window
+            .emit(
+                "monitoring_data",
+                Payload {
+                    message: rust_string.into(),
+                },
+            )
+            .unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(polling_rate));
+    });
+}
+
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
+            // let main_window = app.get_window("main").unwrap();
             // let polling_rate: u64 = 2000;
-            let id = app.listen_global("start_monitoring", |event| {
-                println!("got event-name with payload {:?}", event.payload());
-                // polling_rate = event.payload()
-            });
-
-            app.unlisten(id);
-            // TODO: send result of this method
-            unsafe {
-                let c_string = CString::new("Cpu,GpuNvidia").unwrap();
-
-                // let result = GetHardwareInfo(c_string.as_ptr());
-                let result = call_dynamic(c_string.as_ptr()).unwrap();
-                println!("{:?}", CStr::from_ptr(result))
-            };
-            //     app.emit_all(
-            //         "event-name",
-            //         Payload {
-            //             message: "Tauri is awesome!".into(),
-            //         },
-            //     )
-
-            // std::thread::spawn(move || {
-            // loop {
-            //     // Replace this with your data to send to the frontend
-            //     let data = "Your data to send";
-
-            //     app.emit_all(
-            //         "event-name",
-            //         Payload {
-            //             message: "Tauri is awesome!".into(),
-            //         },
-            //     )
-            //     .unwrap();
-
-            //     // Sleep for 1 second
-            //     sleep(Duration::from_millis(polling_rate));
-            // }
+            // let id = app.listen_global("start-monitoring", |event| {
+            //     println!("got event-name with payload {:?}", event.payload());
             // });
+
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![init_process])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
